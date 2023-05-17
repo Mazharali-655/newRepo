967ecc7c622f0bc17ead3dff3c2f858a412d1e4c
Mazharali-655 <mazharalikalwar655@gmail.com>
2023-05-18 01:56:49 +0500

    class 1

---
+++ app.js
@@ -0,0 +1,40 @@
+// let name = "mazhar ali"; //Variable
+// alert(name);
+
+// variable Rules
+// 1.legal or Illegal names
+
+    //Legal
+    // use camelcase for long variable names
+    //let lastName // camelcase
+    // Allow special charaters like
+    // _ $
+    // let $nameWith$Dollar = "";
+    // let _nameWith_Dollar = "";
+    // var cannot be start with a number
+    // let myName
+
+
+     //Illegal 
+     // let last name //spases
+     // let my#name
+
+     //alert(firstName);
+
+
+        //pratice
+        
+    //  let _firstName = "Mazhar";
+    //  alert (_firstName);
+
+    //  let $lastName = "Ali";
+    //  alert ($lastName);
+
+    // let 22firstName = "Mazhar";  // not working
+    // alert (_firstName);
+
+    // let last Name = "Ali";   // not working
+    // alert ($lastName);
+    
+    // let _firstName = "Mazhar";   // its workin not use this type.
+    // alert (_firstName);    
